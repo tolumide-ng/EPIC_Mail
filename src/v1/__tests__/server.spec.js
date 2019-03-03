51beaa8970401702698f56ed965479538825fd40
Tolumide <tolumideshopein@gmail.com>
2019-03-03 12:20:30 +0100

    [Feature #164352786] Add Tests signup endpoint

--- src/v1/__tests__/server.spec.js
+++ src/v1/__tests__/server.spec.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import server from './../server';
+import server from './../../server';
 
 chai.use(chaiHttp);
 
@@ -9,25 +9,21 @@ const expect = chai.expect;
 
 
 describe('Server.js endpoints', () => {
-    
-});
-
-describe ('Server.js endpoints', () => {
     it('should return a status of 200', (done) => {
         chai.request(server)
-        .get('/')
-        .end((err, res) => {
-            res.should.be.json;
-            res.should.have.status(200);
-            done();
-        })
+            .get('/')
+            .end((err, res) => {
+                res.should.be.json;
+                res.should.have.status(200);
+                done();
+            })
     });
 
     it('should return a status code of 500', (done) => {
         chai.request(server)
             .get('/aNonExistingRoute')
             .end((req, res) => {
-                res.should.have.status(500);
+                res.should.have.status(400);
                 res.should.be.json;
                 done();
             })

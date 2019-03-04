72c140f0587bc202c655642e6a9c7eb7a7ba6774
Tolumide <tolumideshopein@gmail.com>
2019-03-04 08:01:01 +0100

    [Feature #164357588] Add passportJWT for compose Mail authentication

--- src/v1/routes/messagesRoutes.js
+++ src/v1/routes/messagesRoutes.js
@@ -2,9 +2,26 @@ import express from 'express';
 import passport from 'passport';
 
 import messagesControllers from './../controllers/messagesControllers';
+import messageHelpers from './../helpers/messagesHelpers';
+const { validateBody, schemas } = messageHelpers;
+import passportConf from './../passport';
+
+// For passport custom authenitcation
+const passportJWT = function (req, res, next) {
+    passport.authenticate('jwt', { session: false }, function (err, userExist, info) {
+        if (err) { return next(err) }
+        if (!userExist) {
+            return res.status(401).json({ status: 401, error: 'Auth Error, Email and Password does not match' });
+        }
+    })(req, res, next)
+    next();
+}
+
+
+
 
 const router = express.Router();
 
-router.post('/', messagesControllers.createMessage);
+router.post('/', passportJWT, messagesControllers.createMessage);
 
 export default router;

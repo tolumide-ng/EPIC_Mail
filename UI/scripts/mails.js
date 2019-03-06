7b70162212526639249d6477a126e1a79e50681c
Tolumide <tolumideshopein@gmail.com>
2019-03-06 03:27:57 +0100

    feat(user): enhance mail menu
    - inbox messages displays when the mail menu is clicked on the navigation bar
    
    [Delivers #164411506]

--- UI/scripts/mails.js
+++ UI/scripts/mails.js
@@ -1,15 +1,33 @@
 const menuList = document.querySelector('#menuList');
 const displayContainer = document.querySelector('#displayContainer');
+function inboxMessagesDisplay() {
+    const inboxMessageContainer = displayContainer.querySelector('.inboxMessagesContainer');
+    const inboxMessage = displayContainer.querySelectorAll('.inboxMessage');
+    inboxMessageContainer.addEventListener('click', e => {
+        console.log(inboxMessage);
+        const theinboxMessage = e.target.closest('.inboxMessage');
+        if (theinboxMessage) {
+            theinboxMessage.style.backgroundColor = 'blue';
+            setTimeout(() => {
+                theinboxMessage.style.backgroundColor = '';
+            }, 0);
+            displayContainer.innerHTML = '';
+            theinboxMessage.lastElementChild.classList.toggle('visibility');
 
-// Compose Mail appears first when the Mail menu is clicked, so the action buttons should respond
-displayContainer.querySelector('#draftButton').addEventListener('click', e => {
-    displayContainer.innerHTML = '';
-    displayContainer.innerHTML = `<strong>Message saved as draft</strong>`
-});
-displayContainer.querySelector('#sendButton').addEventListener('click', e => {
-    displayContainer.innerHTML = '';
-    displayContainer.innerHTML = `<strong>Message sent</strong>`
-});
+            displayContainer.innerHTML = `
+        <div class='spaceContent'><strong> To: </strong> ${theinboxMessage.children[0].textContent} </div>
+
+        <div class='spaceContent'> <strong> Subject: </strong> ${theinboxMessage.children[1].textContent} </div>
+
+        <div class='spaceContent'> <strong> Message: </strong> <div class='theMessageDisplayed'> ${theinboxMessage.lastElementChild.textContent} </div> </div>
+        `;
+            console.log(theinboxMessage.children);
+        }
+    });
+}
+
+// Email inbox appears first when the Mail menu is clicked, so the action buttons should respond
+inboxMessagesDisplay();
 
 
 // Toggle the feature in view to the clicked menu content when any of the page specific menu is clicked
@@ -38,48 +56,25 @@ menuList.addEventListener('click', e => {
     const selectedContent = e.target.closest('.menuListContent').getAttribute('id');
 
     if (selectedContent === 'inbox') {
-        // INBOX MAIL
-        const inboxMessageContainer = displayContainer.querySelector('.inboxMessagesContainer');
-        const inboxMessage = displayContainer.querySelectorAll('.inboxMessage');
-        inboxMessageContainer.addEventListener('click', e => {
-            console.log(inboxMessage);
-            const theinboxMessage = e.target.closest('.inboxMessage');
-            if (theinboxMessage) {
-                theinboxMessage.style.backgroundColor = 'blue';
-                setTimeout(() => {
-                    theinboxMessage.style.backgroundColor = '';
-                }, 0);
-                displayContainer.innerHTML = '';
-                theinboxMessage.lastElementChild.classList.toggle('visibility');
-
-                displayContainer.innerHTML = `
-        <div class='spaceContent'><strong> To: </strong> ${theinboxMessage.children[0].textContent} </div>
-
-        <div class='spaceContent'> <strong> Subject: </strong> ${theinboxMessage.children[1].textContent} </div>
-
-        <div class='spaceContent'> <strong> Message: </strong> <div class='theMessageDisplayed'> ${theinboxMessage.lastElementChild.textContent} </div> </div>
-        `;
-                console.log(theinboxMessage.children);
-            }
-        });
+        inboxMessagesDisplay();
     } else if (selectedContent === 'draft') {
         // DRAFT MAIL
-    const draftMessageContainer = displayContainer.querySelector('.draftMessagesContainer');
-    const draftMessage = displayContainer.querySelectorAll('.draftMessage');
-    draftMessageContainer.addEventListener('click', e => {
-        console.log('welcome to drafts');
-        const theDraftMessage = e.target.closest('.draftMessage');
-        if (theDraftMessage) {
-            theDraftMessage.style.backgroundColor = 'blue';
+        const draftMessageContainer = displayContainer.querySelector('.draftMessagesContainer');
+        const draftMessage = displayContainer.querySelectorAll('.draftMessage');
+        draftMessageContainer.addEventListener('click', e => {
+            console.log('welcome to drafts');
+            const theDraftMessage = e.target.closest('.draftMessage');
+            if (theDraftMessage) {
+                theDraftMessage.style.backgroundColor = 'blue';
 
-            setTimeout(() => {
-                theDraftMessage.style.backgroundColor = '';
-            }, 0);
-        }
+                setTimeout(() => {
+                    theDraftMessage.style.backgroundColor = '';
+                }, 0);
+            }
 
-        displayContainer.innerHTML = '';
-        theDraftMessage.lastElementChild.classList.toggle('visibility');
-        displayContainer.innerHTML = `
+            displayContainer.innerHTML = '';
+            theDraftMessage.lastElementChild.classList.toggle('visibility');
+            displayContainer.innerHTML = `
         <div class='spaceContent'><strong> To: </strong> ${theDraftMessage.children[0].textContent} </div>
 
         <div class='spaceContent'> <strong> Subject: </strong> ${theDraftMessage.children[1].textContent} </div>
@@ -90,28 +85,28 @@ menuList.addEventListener('click', e => {
         <button class='buttonsOfDraft'>send</button> <button class='buttonsOfDraft'>Delete</button>
         </div>
         `;
-        console.log(theDraftMessage.children);
-    });
+            console.log(theDraftMessage.children);
+        });
 
-    } else if(selectedContent === 'sent') {
+    } else if (selectedContent === 'sent') {
         // SENT MAIL
-    const sentMessageContainer = displayContainer.querySelector('.sentMessagesContainer');
-    const sentMessage = displayContainer.querySelectorAll('.sentMessage');
-    sentMessageContainer.addEventListener('click', e => {
-        console.log('welcome to sent mails');
-        if (e.target.closest('.sentMessage')) {
-            e.target.closest('.sentMessage').style.backgroundColor = 'blue';
+        const sentMessageContainer = displayContainer.querySelector('.sentMessagesContainer');
+        const sentMessage = displayContainer.querySelectorAll('.sentMessage');
+        sentMessageContainer.addEventListener('click', e => {
+            console.log('welcome to sent mails');
+            if (e.target.closest('.sentMessage')) {
+                e.target.closest('.sentMessage').style.backgroundColor = 'blue';
 
-            setTimeout(() => {
-                e.target.closest('.sentMessage').style.backgroundColor = '';
-            }, 0);
+                setTimeout(() => {
+                    e.target.closest('.sentMessage').style.backgroundColor = '';
+                }, 0);
 
-            displayContainer.innerHTML = '';
-            // ID of the selected target
-            // displayContainer.append(e.target.closest('.sentMessage').childrenElement);
-            e.target.closest('.sentMessage').lastElementChild.classList.toggle('visibility');
+                displayContainer.innerHTML = '';
+                // ID of the selected target
+                // displayContainer.append(e.target.closest('.sentMessage').childrenElement);
+                e.target.closest('.sentMessage').lastElementChild.classList.toggle('visibility');
 
-            displayContainer.innerHTML = `
+                displayContainer.innerHTML = `
         <div class='spaceContent'><strong> To: </strong> ${(e.target.closest('.sentMessage').children[0].textContent)} </div>
 
         <div class='spaceContent'> <strong> Subject: </strong> ${e.target.closest('.sentMessage').children[1].textContent} </div>
@@ -122,12 +117,12 @@ menuList.addEventListener('click', e => {
         <button class='retract'>Retract message</button>
         </div>
         `;
-            console.log((e.target.closest('.sentMessage').children));
-        }
-        return;
-    });
+                console.log((e.target.closest('.sentMessage').children));
+            }
+            return;
+        });
 
-    } else if(selectedContent === 'compose') {
+    } else if (selectedContent === 'compose') {
         displayContainer.querySelector('#draftButton').addEventListener('click', e => {
             displayContainer.innerHTML = '';
             displayContainer.innerHTML = `<strong>Message saved as draft</strong>`

da30114f72abddfbb1342e6860c656eb0f1be950
Tolumide <tolumideshopein@gmail.com>
2019-03-12 13:05:25 +0100

    chore(user): Reinstall eslint
    - Reinstall eslint on the project and add .eslintignore
    - Add .eslintignore to the source project
    - Add .eslintignore to the .gitignore file in the project
    - eslint all files .js files in the project
    
    [Delivers #164567052]

--- src/server.js
+++ src/server.js
@@ -10,33 +10,30 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api/v1/auth', usersRoutes);
 app.use('/api/v1/messages', messagesRoutes);
 
-app.get('/', (req, res) => {
-    return res.status(200).json({ status: 200, data: "'YAY!' Welcome to EPIC_Mail"});
-});
-
+app.get('/', (req, res) => res.status(200).json({ status: 200, data: "'YAY!' Welcome to EPIC_Mail" }));
 
 
 // Handle unavailable routes
 app.use((req, res, next) => {
-    const error = new Error('Route not listed in endpoints, Not found');
-    error.status = 400;
-    next(error);
-})
+  const error = new Error('Route not listed in endpoints, Not found');
+  error.status = 400;
+  next(error);
+});
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({ status: error.status||500, error: `This error is thrown because ${error.name}, ${error.message}` });
+app.use((error, req, res) => {
+  res.status(error.status || 500);
+  res.json({ status: error.status || 500, error: `This error is thrown because ${error.name}, ${error.message}` });
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => { console.log(`server is running on port ${port}` )});
+app.listen(port, () => { console.log(`server is running on port ${port}`); });
 
 export default app;
--- src/v1/models/usersModels.js
+++ src/v1/models/usersModels.js
@@ -6,58 +6,56 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Token generator function
-const signToken = userId => {
-    return jwt.sign({
-        iss: 'EPIC_Mail',
-        sub: userId,
-        iat: new Date().getTime(),
-        exp: new Date().setDate(new Date().getDate() + 1)
-    }, process.env.SECRET_KEY);
-}
+const signToken = userId => jwt.sign({
+  iss: 'EPIC_Mail',
+  sub: userId,
+  iat: new Date().getTime(),
+  exp: new Date().setDate(new Date().getDate() + 1),
+}, process.env.SECRET_KEY);
 
 // Password hashing function
-const hashPassword = password => {
-    const salt = bcrypt.genSaltSync(10);
-    return bcrypt.hashSync(password, salt);
-}
+const hashPassword = (password) => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+};
 
 class User {
-    constructor() {
-        this.users = [];
-    }
+  constructor() {
+    this.users = [];
+  }
 
-    signup(data) {
-        // Generate the salt
-        const password = hashPassword(data.password);
+  signup(data) {
+    // Generate the salt
+    const password = hashPassword(data.password);
 
-        const newUser = {
-            id: uuid.v4(),
-            email: data.email,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            password: password
-        };
-        // Generate token
-        const token = signToken(newUser.id);
-        this.users.push(newUser);
-        return { token, newUser };
-    }
-
-
-    findUserByEmail(email) {
-        return this.users.find(user => user.email === email);
-    }
+    const newUser = {
+      id: uuid.v4(),
+      email: data.email,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      password,
+    };
+    // Generate token
+    const token = signToken(newUser.id);
+    this.users.push(newUser);
+    return { token, newUser };
+  }
 
 
-    findUserById(id) {
-        return this.users.find(user => user.id === id);
-    }
+  findUserByEmail(email) {
+    return this.users.find(user => user.email === email);
+  }
 
-    userLogin(data) {
-        const foundUser = this.findUserByEmail(data.email);
-        const token = signToken(foundUser.id);
-        return { token, foundUser };
-    }
+
+  findUserById(id) {
+    return this.users.find(user => user.id === id);
+  }
+
+  userLogin(data) {
+    const foundUser = this.findUserByEmail(data.email);
+    const token = signToken(foundUser.id);
+    return { token, foundUser };
+  }
 }
 
 export default new User();

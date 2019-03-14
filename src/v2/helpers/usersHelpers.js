27d3bb27cc16af8fd32a872da95afdcad6fcf9a5
Tolumide <tolumideshopein@gmail.com>
2019-03-14 22:19:33 +0100

    feat(users): Add linting for helpers
    - lint src/v1/helpers/usersHelpers.js
    
    [Delivers #164635014]

--- src/v2/helpers/usersHelpers.js
+++ src/v2/helpers/usersHelpers.js
@@ -2,22 +2,22 @@ import Joi from 'joi';
 
 
 export default {
-    validateBody: schema => (req, res, next) => {
-        const result = Joi.validate(req.body, schema);
-        if (result.error) {
-            return res.status(422).json({ status: 422, error: result.error });
-        }
-        if(!req.value) { req.value = {}; }
-        req.value.body = result.value;
-        next();
-    },
-
-    schemas: {
-        authSchema: Joi.object().keys({
-            email: Joi.string().regex(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/).required(),
-            password: Joi.string().min(3).required(),
-            firstName: Joi.string().required(),
-            lastName: Joi.string().required()
-        })
+  validateBody: schema => (req, res, next) => {
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+      return res.status(422).json({ status: 422, error: result.error });
     }
-}
+    if (!req.value) { req.value = {}; }
+    req.value.body = result.value;
+    next();
+  },
+
+  schemas: {
+    authSchema: Joi.object().keys({
+      email: Joi.string().regex(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/).required(),
+      password: Joi.string().min(3).required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+    }),
+  },
+};

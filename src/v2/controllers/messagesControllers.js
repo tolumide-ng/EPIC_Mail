b933bdd4f51b3093472a1cf3cfabf9ebc57cd24d
Tolumide <tolumideshopein@gmail.com>
2019-03-16 20:35:49 +0100

    feat(users): user can get specific mail
    - registered user can get mails he/she sent/received
    - route to get specific mail
    - controller to get specific mail
    - test cases to get specific mail
    - ignore all files from version-1
    - test coverage only version-2
    - user must be logged in to get specific mail
    
    [Delivers #164679785]

--- src/v2/controllers/messagesControllers.js
+++ src/v2/controllers/messagesControllers.js
@@ -38,6 +38,36 @@ const Mail = {
       }
     })(req, res);
   },
+
+  // Method to get a specific mail
+  async getSpecificMail(req, res) {
+    passport.authenticate('jwt', { session: false }, async (err, user) => {
+      if (err) { return res.status(400).json({ status: 400, error: err }); }
+      if (!user) {
+        return res.status(401).json({ status: 401, error: 'Unauthorized, Email or Password does not match' });
+      }
+      const text = `SELECT * FROM messagesTable 
+      WHERE id=$1 AND (receiverEmail=$2 OR senderEmail=$2)`;
+      const values = [req.params.id, user.email];
+      try {
+        const { rows } = await db.query(text, values);
+        if (!rows[0]) {
+          return res.status(404).json({ status: 404, error: `Not Found, you do not have a message with id=${req.params.id}` });
+        }
+        // If the receiver is the one mjaking a request to the specific message
+        // Then the message status should change to read if it was not yet read
+        if (rows[0].status === 'inbox' && rows[0].receiveremail === user.email) {
+          const updateText = `UPDATE messagesTable
+          SET status=$1`;
+          const updateValue = ['read'];
+          await db.query(updateText, updateValue);
+        }
+        return res.status(200).json({ status: 200, data: [rows[0]] });
+      } catch (error) {
+        return res.status(400).json({ status: 400, error: `${error.name}, ${error.message}` });
+      }
+    })(req, res);
+  },
 };
 
 

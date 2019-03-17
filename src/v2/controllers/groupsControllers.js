8fd2b85c07d1692384fc53670d50d762d5b645a8
Tolumide <tolumideshopein@gmail.com>
2019-03-17 12:22:10 +0100

    feat(user): Edit the name of a specific group
    - create route to the name of a specific group
    - create controller to edit the name of a specific group
    - create dummy data to edit the name of a specific group
    - write test for possible cases to be encountered in editing a group's name
    
    [Delivers #164682631]

--- src/v2/controllers/groupsControllers.js
+++ src/v2/controllers/groupsControllers.js
@@ -44,6 +44,36 @@ const Group = {
         })(req, res);
     },
 
+    async editGroupName(req, res) {
+        passport.authenticate('jwt', {session: false}, async(err, user) => {
+            if(err) { return res.status(400).json({ status: 400, error: err })};
+            if(!user) {
+                return res.status(401).json({ status: 401, error: 'Unauthorized, Email or Password does not match' })
+            }
+            const { id, name } = req.params;
+            const text = `SELECT * FROM groupTable 
+            WHERE id=$1`;
+            const values = [id];
+            try {
+                const { rows } = await db.query(text, values);
+                if(!rows[0]){
+                    return res.status(404).json({ status: 404, error: 'Not Found: Please check the provided groupId'})
+                }
+                if(rows[0].createdby !== user.email){
+                    return res.status(401).json({status: 401, error: 'Unauthorized, You do not have the authority to rename this group'});
+                }
+                if(rows[0].createdby === user.email){
+                    const text= `UPDATE groupTable SET name=$1 returning *`;
+                    const values = [name];
+                    const {rows} = await db.query(text, values);
+                    return res.status(200).json({status: 200, data: [rows[0]]})
+                }
+            } catch(error) {
+                return res.status(400).json({status: 400, error })
+            }
+        })(req, res);
+    }
+
 }
 
 export default Group;
